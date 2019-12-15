54a9f565436c4414b67f30032c0fb8421ce199b5
IslamRustamov <islamrustamov@rambler.ru>
2019-12-16 01:45:32 +0300

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  devise_for :users
   # Mime::SET.collect(&:to_s) shows supported media types
   # I set JSON as default
   namespace :api, defaults: { format: :json },
