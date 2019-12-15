0dd53ccd4c440a5dcfa824cfdf1526fc86a6f92d
IslamRustamov <islamrustamov@rambler.ru>
2019-12-16 01:13:22 +0300

    Set the routes constraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Mime::SET.collect(&:to_s) shows supported media types
+  # I set JSON as default
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/' do
 
-
+  end
 end
