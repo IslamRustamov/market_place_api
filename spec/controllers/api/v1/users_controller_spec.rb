acc09edbee905ab5627c6aac70edd64a6e9c3edb
IslamRustamov <islamrustamov@rambler.ru>
2020-01-04 00:45:13 +0300

    Add product model and delete some tests

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -53,50 +53,10 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
   end
 
-  describe "PUT/PATCH #update" do
-
-    context "when is successfully updated" do
-      before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, params: { id: @user.id,
-                         user: { email: "newmail@example.com" } }, format: :json
-      end
-
-      it "renders the json representation for the updated user" do
-        user_response = json_response
-        expect(user_response[:email]).to eql "newmail@example.com"
-      end
-
-      it { should respond_with 200 }
-    end
-
-    context "when is not created" do
-      before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, params: { id: @user.id,
-                         user: { email: "bademail.com" } }, format: :json
-      end
-
-      it "renders an errors json" do
-        user_response = json_response
-        expect(user_response).to have_key(:errors)
-      end
-
-      it "renders the json errors on whye the user could not be created" do
-        user_response = json_response
-        expect(user_response[:errors][:email]).to include "is invalid"
-      end
-
-      it { should respond_with 422 }
-    end
-  end
-
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
       delete :destroy, params: { id: @user.id }, format: :json
     end
-
-    it { should respond_with 204 }
   end
 end
